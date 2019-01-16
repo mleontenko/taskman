e9d767c2340a21eeaac356daca8a278b46aa5db0
Marin Leontenko <marin.leontenko@gmail.com>
2019-01-16 13:25:00 +0100

    prikaz projekata, prilagođen App.js

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Header from './Header'
+    import ReactDOM from 'react-dom'
+    import { BrowserRouter, Route, Switch } from 'react-router-dom'
+    import Header from './Header'
+    import ProjectsList from './ProjectsList'
 
-class App extends Component {
-    render () {
-    return (
-        <BrowserRouter>
-        <div>
-            <Header />
-        </div>
-        </BrowserRouter>
-    )
+    class App extends Component {
+      render () {
+        return (
+          <BrowserRouter>
+            <div>
+              <Header />
+              <Switch>
+                <Route exact path='/' component={ProjectsList} />
+              </Switch>
+            </div>
+          </BrowserRouter>
+        )
+      }
     }
-}
 
-ReactDOM.render(<App />, document.getElementById('app'))
+    ReactDOM.render(<App />, document.getElementById('app'))
