b9338eeffa18fc81e58c578c668d3d823cbd3cc0
Marin Leontenko <marin.leontenko@gmail.com>
2019-01-16 12:50:42 +0100

    formatiranje koda

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-    import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-    const Header = () => (
-      <nav className='navbar navbar-expand-md navbar-light navbar-laravel'>
-        <div className='container'>
-          <Link className='navbar-brand' to='/'>Tasksman</Link>
-        </div>
-      </nav>
-    )
+const Header = () => (
+    <nav className='navbar navbar-expand-md navbar-light navbar-laravel'>
+    <div className='container'>
+        <Link className='navbar-brand' to='/'>Tasksman</Link>
+    </div>
+    </nav>
+)
 
-    export default Header
+export default Header
